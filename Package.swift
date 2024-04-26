583ee33251b8c711703c4b6a156e0f3d9e9007fc
Ilnur Mugaev <ilnur.mugaev@akbarsdigital.ru>
2024-04-26 21:26:28 +0300

    PCABO3-10518: Добавил пакеты.

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iOS.Resources.ReusableUI",
+    products: [
+        .library(
+            name: "iOS.Resources.ReusableUI",
+            targets: [
+                "Modules"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-colors.git", exact: "0.0.3"),
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-ui-components.git", exact: "0.0.40"),
+    ],
+    targets: [
+        .target(
+            name: "Modules",
+            dependencies: [
+                .product(name: "iOS.Resources.Colors", package: "ios-resources-colors"),
+                .product(name: "iOS.Resources.UiComponents", package: "ios-resources-ui-components"),
+            ]
+        ),
+    ]
+)
