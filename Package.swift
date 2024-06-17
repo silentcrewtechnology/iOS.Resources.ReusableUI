f63c2b67141e20bb3a41a6a7ac3d54d9ea19e14a
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-06-17 16:18:04 +0300

    PCABO3-10861: Обновил версию ДС

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://gitlab.akbars.tech/abo/ios.designsystem", .upToNextMinor(from: "4.0.1")),
+        .package(url: "https://gitlab.akbars.tech/abo/ios.designsystem", .upToNextMinor(from: "4.0.3")),
         .package(url: "https://gitlab.akbars.tech/abo/ios.architecture", .upToNextMinor(from: "0.0.6")),
         .package(url: "https://github.com/onevcat/Kingfisher", exact: "7.11.0"),
     ],
